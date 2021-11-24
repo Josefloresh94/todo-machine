60b1f5a05b7d064853bb362bf13798840f389ffe
Jose Flores <charlijflo@gmail.com>
2021-11-24 10:39:26 -0600

    Completando y eliminando TODOs mejorado

--- src/App.js
+++ src/App.js
@@ -36,7 +36,11 @@ function App() {
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
-    newTodos[todoIndex].completed = true;
+    if(!newTodos[todoIndex].completed) {
+      newTodos[todoIndex].completed = true
+    } else {
+      newTodos[todoIndex].completed = false
+    }
     setTodos(newTodos);
   };
 
