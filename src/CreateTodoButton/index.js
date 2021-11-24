bbef558089d376cc644758afba58ff9d654c022a
Jose Flores <charlijflo@gmail.com>
2021-11-24 17:28:47 -0600

    Formulario para crear TODOs

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { TodoContext } from "../TodoContext";
 import "./CreateTodoButton.css"
 
-function CreateTodoButton () {
-    const { openModal, setOpenModal } = React.useContext(TodoContext);
-
+function CreateTodoButton (props) {
     const onClickButton = () => {
-        setOpenModal(!openModal);
+        props.setOpenModal(prevState => !prevState);
     };
     return (
         <button 
